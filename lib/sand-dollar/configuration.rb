db4aa807682ca0762534209891a880977835dbfc
Dana Franklin <danaf@mediaartslab.com>
2015-02-13 16:12:58 -0800

    Support for storage and user model in config.

--- lib/sand-dollar/configuration.rb
+++ lib/sand-dollar/configuration.rb
@@ -1,7 +1,9 @@
 module SandDollar
   class Configuration
     attr_reader :session_lifetime,
-                :token_length
+                :token_length,
+                :storage,
+                :user_model
 
     def initialize
       # Number of seconds of inactivity a session will survive
@@ -9,6 +11,12 @@ module SandDollar
 
       # Number of seconds of inactivity a session will survive
       self.token_length = SandDollar::Default::TOKEN_LENGTH
+
+      # Persistence strategy [:none, :mongoid]
+      self.storage = SandDollar::Default::STORAGE
+
+      # Name of Model to represent authenticatable users
+      self.user_model = SandDollar::Default::USER_MODEL
     end
 
     def session_lifetime=(int)
@@ -29,6 +37,27 @@ module SandDollar
       @token_length = int
     end
 
+    def storage=(value)
+      value = value.to_sym
+
+      unless SandDollar::Default::VALID_STORAGE.include?(value)
+        raise SandDollar::InvalidStorageConfig
+        return
+      end
+
+      @storage = value
+    end
+
+    def user_model=(value)
+      cleaned = value.to_s.strip
+
+      unless cleaned.length > 0
+        warn "SandDollar user_model cannot be blank. Using current value: #{@user_model}"
+        return
+      end
+
+      @user_model = cleaned
+    end
   end
 
   class << self
