f687a323f17895863d201065f01ebce4983158a0
Dana Franklin <danaf@mediaartslab.com>
2015-02-18 18:06:08 -0800

    Reorganized config tests by config option

--- spec/sand-dollar_spec.rb
+++ spec/sand-dollar_spec.rb
@@ -19,12 +19,12 @@ describe SandDollar::Configuration do
     end
   end
 
-  describe "invalid config" do
+  describe "#configure" do
     after do
       SandDollar.reset_configuration
     end
 
-    describe "for session_lifetime" do
+    describe "session_lifetime" do
       before do
         SandDollar.configure do |config|
           config.session_lifetime = session_lifetime
@@ -33,6 +33,14 @@ describe SandDollar::Configuration do
 
       context "when provided a String for session_lifetime" do
         let(:session_lifetime) { 'Batman' }
+
+        it "uses the default session_lifetime" do
+          expect(SandDollar.configuration.session_lifetime).to eq(SandDollar::Default::SESSION_LIFETIME)
+        end
+      end
+
+      context "when provided a Symbol for session_lifetime" do
+        let(:session_lifetime) { :foobar }
 
         it "uses the default session_lifetime" do
           expect(SandDollar.configuration.session_lifetime).to eq(SandDollar::Default::SESSION_LIFETIME)
@@ -63,83 +71,143 @@ describe SandDollar::Configuration do
         end
       end
 
-    end
+      context "when provided a valid session_lifetime" do
+        let(:session_lifetime) { 180 }
 
-
-    context "when provided an invalid token_length" do
-      before do
-        SandDollar.configure do |config|
-          config.token_length = 'Robin'
+        it "uses the provided session_lifetime" do
+          expect(SandDollar.configuration.session_lifetime).to eq(session_lifetime)
         end
       end
 
-      it "uses the default token_length" do
-        expect(SandDollar.configuration.token_length).to eq(SandDollar::Default::TOKEN_LENGTH)
-      end
     end
 
-    context "when provided an invalid user_model" do
+    describe "token_length" do
       before do
         SandDollar.configure do |config|
-          config.user_model = '             '
-        end
-      end
-
-      it "uses the default user_model" do
-        expect(SandDollar.configuration.user_model).to eq(SandDollar::Default::USER_MODEL)
-      end
-    end
-
-    context "when provided an invalid storage strategy" do
-      it "raises an exception" do
-        expect {
-          SandDollar.configure do |config|
-            config.storage = :foobar
-          end
-        }.to raise_error SandDollar::InvalidStorageConfig
-      end
-    end
-
-  end
-
-  describe "#configure" do
-    after do
-      SandDollar.reset_configuration
-    end
-
-    context "when provided a valid config" do
-
-      before do
-        SandDollar.configure do |config|
-          config.session_lifetime = session_lifetime
           config.token_length = token_length
-          config.user_model = user_model
-          config.storage = storage
         end
       end
 
-      let(:session_lifetime) { 180 }
-      let(:token_length) { 512 }
-      let(:user_model) { 'AdminUser' }
-      let(:storage) { 'Mongoid' }
+      context "when provided a String for token_length" do
+        let(:token_length) { 'Superman' }
 
-      it "configures the session_lifetime" do
-        expect(SandDollar.configuration.session_lifetime).to eq(session_lifetime)
+        it "uses the default token_length" do
+          expect(SandDollar.configuration.token_length).to eq(SandDollar::Default::TOKEN_LENGTH)
+        end
       end
 
-      it "configures the token_length" do
-        expect(SandDollar.configuration.token_length).to eq(token_length)
+      context "when provided a Symbol for token_length" do
+        let(:token_length) { :punkrock }
+
+        it "uses the default token_length" do
+          expect(SandDollar.configuration.token_length).to eq(SandDollar::Default::TOKEN_LENGTH)
+        end
       end
 
-      it "configures the user_model" do
-        expect(SandDollar.configuration.user_model).to eq(:admin_user)
+      context "when provided nil for token_length" do
+        let(:token_length) { nil }
+
+        it "uses the default token_length" do
+          expect(SandDollar.configuration.token_length).to eq(SandDollar::Default::TOKEN_LENGTH)
+        end
       end
 
-      it "configures the storage" do
-        expect(SandDollar.configuration.storage).to eq(:mongoid)
+      context "when provided 0 for token_length" do
+        let(:token_length) { 0 }
+
+        it "uses the default token_length" do
+          expect(SandDollar.configuration.token_length).to eq(SandDollar::Default::TOKEN_LENGTH)
+        end
+      end
+
+      context "when provided a negative integer for token_length" do
+        let(:token_length) { -10 }
+
+        it "uses the default token_length" do
+          expect(SandDollar.configuration.token_length).to eq(SandDollar::Default::TOKEN_LENGTH)
+        end
+      end
+
+      context "when provided a valid token_length" do
+        let(:token_length) { 180 }
+
+        it "uses the provided token_length" do
+          expect(SandDollar.configuration.token_length).to eq(token_length)
+        end
+      end
+    end
+
+    describe "user_model" do
+      before do
+        SandDollar.configure do |config|
+          config.user_model = user_model
+        end
+      end
+
+      context "when provided an empty String for user_model" do
+        let(:user_model) { '      ' }
+
+        it "uses the default user_model" do
+          expect(SandDollar.configuration.user_model).to eq(SandDollar::Default::USER_MODEL)
+        end
+      end
+
+      context "when provided nil for user_model" do
+        let(:user_model) { nil }
+
+        it "uses the default user_model" do
+          expect(SandDollar.configuration.user_model).to eq(SandDollar::Default::USER_MODEL)
+        end
+      end
+
+      context "when provided an Integer for user_model" do
+        let(:user_model) { 100 }
+
+        it "uses the default user_model" do
+          expect(SandDollar.configuration.user_model).to eq(SandDollar::Default::USER_MODEL)
+        end
+      end
+
+      context "when provided a String for user_model" do
+        let(:user_model) { 'Author' }
+
+        it "uses a symbol equivalent of the provided user_model" do
+          expect(SandDollar.configuration.user_model).to eq(:author)
+        end
+      end
+
+      context "when provided a Symbol for user_model" do
+        let(:user_model) { :admin_user }
+
+        it "uses the provided user_model" do
+          expect(SandDollar.configuration.user_model).to eq(user_model)
+        end
+      end
+    end
+
+    describe "storage" do
+      context "when provided an invalid storage strategy" do
+        it "raises an exception" do
+          expect {
+            SandDollar.configure do |config|
+              config.storage = :foobar
+            end
+          }.to raise_error SandDollar::InvalidStorageConfig
+        end
+      end
+
+      context "when provided a valid storage strategy" do
+        it "uses a symbol equivalent of the provided storage strategy" do
+          SandDollar.configure do |config|
+            config.storage = 'Mongoid'
+          end
+
+          expect(SandDollar.configuration.storage).to eq(:mongoid)
+        end
       end
 
     end
 
   end
+
 end
