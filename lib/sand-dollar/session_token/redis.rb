058b81d207c5a50a78c01552ca8a8672544aaa08
Dana Franklin <danaf@mediaartslab.com>
2015-02-23 14:01:57 -0800

    Refactor repetitive strings into single methods

--- lib/sand-dollar/session_token/redis.rb
+++ lib/sand-dollar/session_token/redis.rb
@@ -26,16 +26,24 @@ module SandDollar::SessionToken
       def token_exists?(token)
         token = token.to_s.strip
         return false unless token.length > 0
-        store.exists("token/#{token}")
+        store.exists(storage_key_for(token))
       end
 
       def discard(token)
-        store.del("token/#{token}")
+        store.del(storage_key_for(token))
       end
 
       def discard_all
-        keys = store.keys("token/*")
+        keys = store.keys("#{storage_key_prefix}/*")
         store.del(*keys) if keys && keys.count > 0
+      end
+
+      def storage_key_for(token)
+        "#{storage_key_prefix}/#{token}"
+      end
+
+      def storage_key_prefix
+        'token'
       end
 
     end
@@ -50,7 +58,7 @@ module SandDollar::SessionToken
         session_data[:updated_at]
       end
 
-      def user_id
+      def identify_user
         session_data[self.class.token_user_id_field]
       end
 
@@ -60,21 +68,21 @@ module SandDollar::SessionToken
       end
 
       def save
-        store.set("token/#{token}", session_data.to_json)
-        store.expire(token, ttl.ceil)
+        _set_with_expire(self.class.storage_key_for(token), session_data.to_json)
         self
       end
 
       def load_token(token)
         token = token.to_s.strip
-        return self unless token.length > 0 && store.exists("token/#{token}")
+        return self unless token.length > 0 && store.exists(self.class.storage_key_for(token))
 
-        result = store.get("token/#{token}")
+        result = store.get(self.class.storage_key_for(token))
         return self unless result.is_a?(String)
 
+        @token = token
         @session_data = JSON.parse(result, :symbolize_names => true)
         @session_data[:updated_at] = Time.parse(@session_data[:updated_at])
-        @token = token
+
         self
       end
 
@@ -92,6 +100,17 @@ module SandDollar::SessionToken
 
       def session_data
         @session_data ||= {}
+      end
+
+      def storage_key
+        self.class.storage_key_for(token)
+      end
+
+      def _set_with_expire(key, value)
+        return false if value.to_s.empty? || expired?
+        store.set(key, value)
+        store.expire(key, ttl.ceil)
+        true
       end
     end
 
