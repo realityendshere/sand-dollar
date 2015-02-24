83195dacc992242cd627dbd4c78d47243542a1e2
Dana Franklin <danaf@mediaartslab.com>
2015-02-23 17:35:19 -0800

    Store token data in separate keys in Redis

--- lib/sand-dollar/session_token/redis.rb
+++ lib/sand-dollar/session_token/redis.rb
@@ -26,11 +26,13 @@ module SandDollar::SessionToken
       def token_exists?(token)
         token = token.to_s.strip
         return false unless token.length > 0
-        store.exists(storage_key_for(token))
+        keys = store.keys("#{storage_key_for(token)}/*")
+        keys && keys.count > 0
       end
 
       def discard(token)
-        store.del(storage_key_for(token))
+        keys = store.keys("#{storage_key_for(token)}/*")
+        store.del(*keys) if keys && keys.count > 0
       end
 
       def discard_all
@@ -55,33 +57,30 @@ module SandDollar::SessionToken
     module InstanceMethods
 
       def updated_at
-        session_data[:updated_at]
+        @updated_at ||= _retrieve_updated_at
       end
 
       def identify_user
-        session_data[self.class.token_user_id_field]
+        @identity ||= _retrieve_identity
       end
 
       def authenticate_as user_instance
-        session_data[self.class.token_user_id_field] = user_instance.respond_to?(:id) ? user_instance.id : user_instance
+        @identity = user_instance.respond_to?(:id) ? user_instance.id : user_instance
         self.save
       end
 
       def save
-        _set_with_expire(self.class.storage_key_for(token), session_data.to_json)
+        _set_with_expire(storage_key_identity, identify_user)
+        _set_with_expire(storage_key_updated_at, updated_at)
         self
       end
 
       def load_token(token)
         token = token.to_s.strip
-        return self unless token.length > 0 && store.exists(self.class.storage_key_for(token))
+        return self unless token.length > 0 && self.class.token_exists?(token)
 
-        result = store.get(self.class.storage_key_for(token))
-        return self unless result.is_a?(String)
-
+        @updated_at = _retrieve_updated_at
         @token = token
-        @session_data = JSON.parse(result, :symbolize_names => true)
-        @session_data[:updated_at] = Time.parse(@session_data[:updated_at])
 
         self
       end
@@ -89,8 +88,7 @@ module SandDollar::SessionToken
       protected
 
       def update_activity_timestamp
-        session_data[:updated_at] = Time.now
-        self.save
+        @updated_at = Time.now
         self
       end
 
@@ -98,19 +96,32 @@ module SandDollar::SessionToken
         self.class.store
       end
 
-      def session_data
-        @session_data ||= {}
-      end
-
       def storage_key
         self.class.storage_key_for(token)
       end
 
       def _set_with_expire(key, value)
-        return false if value.to_s.empty? || expired?
+        return false if value.to_s.empty?
         store.set(key, value)
         store.expire(key, ttl.ceil)
         true
+      end
+
+      def _retrieve_updated_at
+        ts = store.get(storage_key_updated_at)
+        Time.parse(ts) rescue nil
+      end
+
+      def _retrieve_identity
+        store.get(storage_key_identity)
+      end
+
+      def storage_key_updated_at
+        "#{storage_key}/updated_at"
+      end
+
+      def storage_key_identity
+        "#{storage_key}/#{self.class.token_user_id_field}"
       end
     end
 
