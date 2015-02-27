ebe5c166d3a03dc055f90832b2ba10556c3e15ea
Dana Franklin <danaf@mediaartslab.com>
2015-02-26 18:28:48 -0800

    gem exceptions NotAuthorized, AuthenticationFailed

--- lib/sand-dollar/exceptions.rb
+++ lib/sand-dollar/exceptions.rb
@@ -7,4 +7,20 @@ module SandDollar
 
   end
 
+  class NotAuthorized < Exception
+
+    def to_s
+      "Not Authorized"
+    end
+
+  end
+
+  class AuthenticationFailed < Exception
+
+    def to_s
+      "That username and password could not be authenticated."
+    end
+
+  end
+
 end
