5c4c8cd7a729014fef50bbe85a7ef16da93b4fbc
Dana Franklin <danaf@mediaartslab.com>
2015-03-06 18:28:26 -0800

    Refactor authenticator validation

--- lib/sand-dollar/authenticators.rb
+++ lib/sand-dollar/authenticators.rb
@@ -5,21 +5,14 @@ module SandDollar
 
       def add(key, authenticator = nil)
         authenticator ||= Class.new(SandDollar::Authenticators::Base)
-
-        unless authenticator.method_defined?(:authenticate!)
-          raise NoMethodError, "authenticate! is not declared in the #{key.inspect} authenticator"
-        end
-
-        base = SandDollar::Authenticators::Base
-        unless authenticator.ancestors.include?(base)
-          raise "#{key.inspect} is not a #{base}"
-        end
-
-        _authenticators[key] = authenticator
+        _authenticators[key] = authenticator if validate_authenticator!(authenticator)
       end
 
       def load(key)
-        authenticator "#{key.to_s}_authenticator"
+        return _authenticators[key] if _authenticators[key]
+        auth = _authenticator("#{key.to_s}_authenticator")
+        add(key, auth)
+        return _authenticators[key]
       end
 
       def [](key)
@@ -54,11 +47,24 @@ module SandDollar
         _authenticator_directory(*args) or raise SandDollar::AuthenticatorNotFound
       end
 
-      def authenticator(name)
+      def _authenticator(name)
         authenticator_class = Object.const_get(name.to_s.classify) rescue nil
         return authenticator_class unless authenticator_class.nil?
         require("#{authenticator_directory!(name)}/#{name}")
         Object.const_get(name.to_s.classify) or raise SandDollar::AuthenticatorNotFound
+      end
+
+      def validate_authenticator!(auth)
+        unless auth.method_defined?(:authenticate!)
+          raise NoMethodError, "authenticate! is not declared in the #{key.inspect} authenticator"
+        end
+
+        base = SandDollar::Authenticators::Base
+        unless auth.ancestors.include?(base)
+          raise "#{key.inspect} is not a #{base}"
+        end
+
+        true
       end
 
     end
